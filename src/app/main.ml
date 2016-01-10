be806fa81b9859b1f09da729118c58721f99892b
Edgar Aroutiounian <edgar.factorial@gmail.com>
2016-01-10 02:23:34 -0800

    Bump version, add more descr

--- src/app/main.ml
+++ src/app/main.ml
@@ -161,7 +161,7 @@ let top_level_info =
              `S "AUTHOR";
              `P "Edgar Aroutiounian"]
   in
-  Term.info "gandalf" ~version:"0.2" ~doc ~man
+  Term.info "gandalf" ~version:"0.4" ~doc ~man
 
 let () =
   match Term.eval (entry_point, top_level_info) with
