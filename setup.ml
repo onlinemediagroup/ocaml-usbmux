e559642fbab53508007a53dc85e0cb232cb20a59
Edgar Aroutiounian <edgar.factorial@gmail.com>
2016-07-21 11:49:18 -0700

    Ppx upstream changed, changed variants and had to update accordingly

--- setup.ml
+++ setup.ml
@@ -1,4 +1,4 @@
-(* setup.ml generated for the first time by OASIS v0.4.5 *)
+(* setup.ml generated for the first time by OASIS v0.4.6 *)
 
 (* OASIS_START *)
 (* DO NOT EDIT (digest: 9852805d5c19ca1cb6abefde2dcea323) *)
